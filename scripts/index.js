eb77f2be42ff44f8d068ecd317fbdba4f8374999
Kirill <sergeew_k@mail.ru>
2022-01-13 14:21:15 +0300

    btn Text change

--- scripts/index.js
+++ scripts/index.js
@@ -2,9 +2,16 @@ const btnShowHide = document.querySelector('.btn-show-hide')
 const spanText = document.querySelector('.span-text')
 const blockForSpace = document.getElementById('block')
 let blockExist = true
+let spanTextVisible = true
 
 btnShowHide.addEventListener('click', () => {
   spanText.classList.toggle('invisible')
+  if (spanTextVisible === true) {
+    btnShowHide.textContent = 'Show text'
+    spanTextVisible = false
+  } else {
+    btnShowHide.textContent = 'Hide text'
+  }
 })
 
 window.addEventListener('keydown', (event) => {
